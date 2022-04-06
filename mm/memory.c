54fec4d16b86e2a4b55cbed18326685e734e8187
TsingPeng <TsingPeng@foxmail.com>
2022-04-06 15:08:01 +0800

    drivers: staging: Add support for miui reclaim optimization
    Signed-off-by: TsingPeng <TsingPeng@foxmail.com>

--- mm/memory.c
+++ mm/memory.c
@@ -2,6 +2,7 @@
  *  linux/mm/memory.c
  *
  *  Copyright (C) 1991, 1992, 1993, 1994  Linus Torvalds
+ *  Copyright (C) 2021 XiaoMi, Inc.
  */
 
 /*
@@ -1349,7 +1350,6 @@ again:
 		pte_t ptent = *pte;
 		if (pte_none(ptent))
 			continue;
-
 		if (need_resched())
 			break;
 
